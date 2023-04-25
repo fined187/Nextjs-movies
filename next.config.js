9a9b47e14c859301af9e96f4b61b9f5069d332d9
fined000@naver.com <fined000@naver.com>
2023-04-25 23:04:13 +0900

    api fetch

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const API_KEY = "67acc0056afde49c51c17021ef4100f6";
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirect() {
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
+};
